9f367dde51b00e0a15003d4e9aaa61136fa0660e
Saptarshi Mondal <saptarshi.mondal@gmail.com>
2016-10-04 14:11:48 -0700

    Fixing some issues

--- init.el
+++ init.el
@@ -22,14 +22,14 @@
 (load "emacs-sm-theme.el")
 
 ;; Set up el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+;;(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;;(unless (require 'el-get nil 'noerror)
+;;  (with-current-buffer
+;;      (url-retrieve-synchronously
+;;       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;;    (let (el-get-master-branch)
+;;      (goto-char (point-max))
+;;      (eval-print-last-sexp))))
 
 ;;Enable Company mode globally
 (add-to-list 'load-path "~/.emacs.d/el-get/company-mode")
